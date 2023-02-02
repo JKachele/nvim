04f509a7ca8bf06f0b59403b7f488d9baa9a19bd
JKachele <justin@kachele.com>
2023-02-02 14:27:00 -0500

    initial commit

---
+++ init.lua
@@ -0,0 +1,8 @@
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.cmp"
+require "user.tabnine"
+require "user.autopairs"
+require "user.comment"
