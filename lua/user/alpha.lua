6bc6b99512f5cf65d67a1581a6cb0b009b9cd0ef
JKachele <justin@kachele.com>
2023-02-04 11:13:35 -0500

    Updated options and alpha footer

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -28,7 +28,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "chrisatmachine.com"
+	return "Justin Kachele"
 end
 
 dashboard.section.footer.val = footer()
