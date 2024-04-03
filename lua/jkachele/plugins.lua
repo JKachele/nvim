7f78cb968f646626c71a3247ea970f7af761c3c7
JKachele <justin@kachele.com>
2024-04-03 08:25:23 -0400

    Organized plugins file and added plugin descriptions

--- lua/jkachele/plugins.lua
+++ lua/jkachele/plugins.lua
@@ -22,99 +22,92 @@ end
 -- Install your plugins here
 lazy.setup({
     -- My plugins here
-    "nvim-lua/popup.nvim",          -- An implementation of the Popup API from vim in Neovim
-    "nvim-lua/plenary.nvim",        -- Useful lua functions used ny lots of plugins
-    "windwp/nvim-autopairs",        -- Autopairs, integrates with both cmp and treesitter
-    "numToStr/Comment.nvim",        -- Easily comment stuff
-    "nvim-tree/nvim-web-devicons",
-    { "nvim-tree/nvim-tree.lua",
+    "nvim-lua/popup.nvim",                      -- An implementation of the Popup API from vim in Neovim
+    "nvim-lua/plenary.nvim",                    -- Useful lua functions used ny lots of plugins
+    "windwp/nvim-autopairs",                    -- Autopairs, integrates with both cmp and treesitter
+    "numToStr/Comment.nvim",                    -- Easily comment stuff
+    "nvim-lualine/lualine.nvim",                -- Status Line on bottom
+    "akinsho/bufferline.nvim",                  -- Line to show open Buffers
+    "moll/vim-bbye",                            -- Easily delete buffers
+    "akinsho/toggleterm.nvim",                  -- Floating Terminal in Nvim
+    "ahmedkhalf/project.nvim",                  -- Easy Project management
+    "goolord/alpha-nvim",                       -- Neovim Title Screen
+    "Shatur/neovim-tasks",                      -- Create tasks to easily compile and run projects
+    "folke/which-key.nvim",                     -- Easily see keybinds
+    { "jkachele/vim-templates",                 -- Add templates for files
+
+        branch = "all"
+    },
+    { "nvim-tree/nvim-tree.lua",                -- Side Panel File Navigation
         version = "*",
         lazy = false,
-        dependencies = {"nvim-tree/nvim-web-devicons"},
+        dependencies = { "nvim-tree/nvim-web-devicons" },
     },
-    "nvim-lualine/lualine.nvim",    -- Status Line on bottom
-    "akinsho/bufferline.nvim",      -- Line to show open Buffers
-    "moll/vim-bbye",                -- Easily delete buffers
-    "akinsho/toggleterm.nvim",      -- Floating Terminal in Nvim
-    "ahmedkhalf/project.nvim",      -- Easy Project management
     { "lukas-reineke/indent-blankline.nvim",    -- Creates an indent line
         main = "ibl",
         opts = {}
     },
-    "goolord/alpha-nvim",           -- Neovim Title Screen
-    "Shatur/neovim-tasks",          -- Create tasks to easily compile and run projects
-    {"jkachele/vim-templates", branch = "all"},  -- Add templates for files
-    "folke/which-key.nvim",         -- Easily see keybinds
-    { "m4xshen/hardtime.nvim",      -- Disable some keybinds to force use of vim binds
-        dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" }
+    { "m4xshen/hardtime.nvim",                  -- Disable some keybinds to force use of vim binds
+        dependencies = { "MunifTanjim/nui.nvim" }
     },
-    { "iamcco/markdown-preview.nvim",   -- Preview Markdown files in the browser
-        build=function() vim.fn["mkdp#util#install"]() end
+    { "iamcco/markdown-preview.nvim",           -- Preview Markdown files in the browser
+        build=function()
+            vim.fn["mkdp#util#install"]()
+        end
     },
-    "mbbill/undotree",              -- See history tree of a file
-    { "stevearc/dressing.nvim",
+    { "stevearc/dressing.nvim",                 -- Enable floating input and selection panels
         event = "VeryLazy",
     },
 
     -- Colorschemes
-    -- "lunarvim/colorschemes", -- A bunch of colorschemes you can try out
-    { "bluz71/vim-moonfly-colors", name = "moonfly" },
+    { "bluz71/vim-moonfly-colors",              -- Moonfly Colorscheme
+        name = "moonfly"
+    },
 
     -- cmp plugins
-    {"hrsh7th/nvim-cmp", commit = "1cad30f"}, -- The completion plugin
-    "hrsh7th/cmp-buffer", -- buffer completions
-    "hrsh7th/cmp-path", -- path completions
-    "hrsh7th/cmp-cmdline", -- cmdline completions
-    "saadparwaiz1/cmp_luasnip", -- snippet completions
+    {"hrsh7th/nvim-cmp", commit = "1cad30f"},   -- The completion plugin
+    "hrsh7th/cmp-buffer",                       -- buffer completions
+    "hrsh7th/cmp-path",                         -- path completions
+    "hrsh7th/cmp-cmdline",                      -- cmdline completions
+    "saadparwaiz1/cmp_luasnip",                 -- snippet completions
     -- {"tzachar/cmp-tabnine", build="./install.sh"}, -- Tabnine completions
-    "hrsh7th/cmp-nvim-lua",
-    "hrsh7th/cmp-nvim-lsp",
-
-    -- snippets
-    "L3MON4D3/LuaSnip", --snippet engine
-    "rafamadriz/friendly-snippets", -- a bunch of snippets to use
+    "hrsh7th/cmp-nvim-lua",                     -- Completions for nvim lua API
+    "hrsh7th/cmp-nvim-lsp",                     -- Completions from lsp
+    "L3MON4D3/LuaSnip",                         --snippet engine
+    "rafamadriz/friendly-snippets",             -- a bunch of snippets to use
 
     -- LSP
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    { "neovim/nvim-lspconfig",
+    "williamboman/mason.nvim",                  -- LSP package manager
+    "williamboman/mason-lspconfig.nvim",        -- Bridges mason.nvim with lspconfig
+    { "neovim/nvim-lspconfig",                  -- Neovim native LSP client cofiguration
         event = { "BufReadPre", "BufNewFile" },
     },
-    "antosha417/nvim-lsp-file-operations",
-    "mfussenegger/nvim-jdtls",
-    "simrat39/rust-tools.nvim",
-    { "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-        config = function()
-            require("lsp_lines").setup()
-        end,
-    },
-    "arkav/lualine-lsp-progress",
+    "mfussenegger/nvim-jdtls",                  -- Java Language Server
+    "ErichDonGubler/lsp_lines.nvim",            -- Inline LSP Diagnostics
+    "arkav/lualine-lsp-progress",               -- Shows LSP loading in Lualine
 
     -- Telescope
-    "nvim-telescope/telescope.nvim",
-    "nvim-telescope/telescope-media-files.nvim",
-    -- { "ThePrimeagen/harpoon",
-    -- branch = "harpoon2",
-    -- dependencies = {"nvim-lua/plenary.nvim"}
-    -- },
+    "nvim-telescope/telescope.nvim",            -- Fuzzy Finder and previewer
+    "nvim-telescope/telescope-media-files.nvim",-- Enable Media previews
 
     -- Treesitter
-    {
-        "nvim-treesitter/nvim-treesitter",
+    { "nvim-treesitter/nvim-treesitter",        -- Better syntax highlighting
         build = ":TSUpdate",
     },
-    -- "p00f/nvim-ts-rainbow",
-    "nvim-treesitter/playground",
-    -- "JoosepAlviste/nvim-ts-context-commentstring",
+    "nvim-treesitter/playground",               -- Easily configure treesitter
 
     -- Git
-    {"lewis6991/gitsigns.nvim", commit = "fb9fd53"},
+    { "lewis6991/gitsigns.nvim",                -- Show Git status and changes
+        commit = "fb9fd53"
+    },
 
     -- Debug Adapter Protocol
-    "mfussenegger/nvim-dap",
-    "rcarriga/nvim-dap-ui",
-    "nvim-telescope/telescope-dap.nvim",
-    "theHamsta/nvim-dap-virtual-text",
-    "jay-babu/mason-nvim-dap.nvim",-- Automatically set up your configuration after cloning packer.nvim
-    "mfussenegger/nvim-dap-python",
+    "mfussenegger/nvim-dap",                    -- Enable debugging
+    { "rcarriga/nvim-dap-ui",                   -- UI for debugging information
+        dependencies = { "nvim-neotest/nvim-nio" },
+    },
+    "nvim-telescope/telescope-dap.nvim",        -- Integrates telescope into dap
+    "theHamsta/nvim-dap-virtual-text",          -- adds virtual text in debugging to show variable values
+    "jay-babu/mason-nvim-dap.nvim",             -- Adds mason functionality to dap
+    "mfussenegger/nvim-dap-python",             -- python debugging
 })
