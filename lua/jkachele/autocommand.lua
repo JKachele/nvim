8a7cded644b5dff1e45440a3de4e58d272c4f5e6
JKachele <justin@kachele.com>
2024-04-15 08:12:52 -0400

    Save current file state when closing a file

--- lua/jkachele/autocommand.lua
+++ lua/jkachele/autocommand.lua
@@ -1,3 +1,6 @@
 vim.cmd "autocmd BufNewFile,BufRead ~/6502Computer/*.asm set ft=oldasm"
 vim.cmd "autocmd BufNewFile,BufRead ~/commands.txt set ft=sh"
 vim.cmd "autocmd BufNewFile,BufRead ~/.config/i3blocks/config set ft=ini"
+
+vim.cmd "autocmd BufWinLeave *.* mkview!"
+vim.cmd "autocmd BufWinEnter *.* silent loadview"
